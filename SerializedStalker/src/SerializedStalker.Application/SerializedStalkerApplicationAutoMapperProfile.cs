9dadc78ee119b00d33c32b6e2a91df30c4204375
Maximuszoo <maximofraisinet1@gmail.com>
2024-09-12 21:32:54 -0300

    Mapeamos las clases y creamos un update

--- SerializedStalker/src/SerializedStalker.Application/SerializedStalkerApplicationAutoMapperProfile.cs
+++ SerializedStalker/src/SerializedStalker.Application/SerializedStalkerApplicationAutoMapperProfile.cs
@@ -1,4 +1,5 @@
 using AutoMapper;
+using SerializedStalker.Series;
 
 namespace SerializedStalker;
 
@@ -9,5 +10,8 @@ public class SerializedStalkerApplicationAutoMapperProfile : Profile
         /* You can configure your AutoMapper mapping configuration here.
          * Alternatively, you can split your mapping configurations
          * into multiple profile classes for a better organization. */
+
+        CreateMap<Serie, SerieDto>();
+        CreateMap<CreateUpdateSerieDto, Serie>();
     }
 }
