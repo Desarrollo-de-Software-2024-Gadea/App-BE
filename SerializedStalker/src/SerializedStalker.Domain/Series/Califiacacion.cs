ae8cefbc9ea8902366d814a98c2e4d6d1ce2954f
TaniJG <86628013+TaniJG@users.noreply.github.com>
2024-10-16 18:19:23 -0300

    Agregados elementos para la calificación y test basico de Buscar Series

---
+++ SerializedStalker/src/SerializedStalker.Domain/Series/Califiacacion.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SerializedStalker.Series
+{
+    public class Califiacacion
+    {
+        public float calificacion {  get; set; }
+        public string comentario { get; set; }
+        public DateTime FechaCreacion { get; set; }
+
+        //Foreign key
+        public int SerieID { get; set; }
+
+    }
+}
---
+++ SerializedStalker/test/SerializedStalker.EntityFrameworkCore.Tests/EntityFrameworkCore/Domains/Series/EfCoreOmdbService_Tests.cs
@@ -0,0 +1,17 @@
+﻿using SerializedStalker.Series;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace SerializedStalker.EntityFrameworkCore.Domains.Series
+{
+
+    [Collection(SerializedStalkerTestConsts.CollectionDefinitionName)]
+    public class EfCoreOmdbService_Tests : OmdbService_Tests<SerializedStalkerEntityFrameworkCoreTestModule>
+    {
+
+    }
+}
