7fd15fbfbe416f9f137c8215f23a823d482c8e3d
Enzo <eftanga@gmail.com>
2024-10-23 20:21:15 -0300

    resolve test error

---
+++ SerializedStalker/test/SerializedStalker.EntityFrameworkCore.Tests/EntityFrameworkCore/Applications/Series/EfCoreSerieAppServiceIntegrationTests.cs
@@ -0,0 +1,17 @@
+﻿using SerializedStalker.Samples;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace SerializedStalker.EntityFrameworkCore.Applications.Series
+{
+    
+    [Collection(SerializedStalkerTestConsts.CollectionDefinitionName)]
+    public class EfCoreSerieAppServiceIntegrationTests : SerieAppServiceIntegrationTests<SerializedStalkerEntityFrameworkCoreTestModule>
+    {
+
+    }
+}
