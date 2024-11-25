9693428e7efb538ff96c7b66e03a915ee46cc686
TaniJG <86628013+TaniJG@users.noreply.github.com>
2024-11-25 18:13:05 -0300

    Comenzada la creación de la parte grafica, pero hay errores no resueltos

--- SerializedStalker/angular/src/app/monitor-api/monitor-api.module.ts
+++ SerializedStalker/angular/src/app/monitor-api/monitor-api.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { MonitoreosComponent } from './monitoreos/monitoreos.component';
 import { SharedModule } from '../shared/shared.module';
+import { MonitoreosComponent } from './monitoreos/monitoreos.component';
 import { MonitorRoutingModule } from './monitor-api-routing.module';
 import { NgxDatatableModule } from '@swimlane/ngx-datatable';
 @NgModule({
